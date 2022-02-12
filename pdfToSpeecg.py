de79d435d0b4a6809972932075fe25ee104de5f6
Ansh Shukla <89148178+shuklansh@users.noreply.github.com>
2022-02-13 04:38:59 +0530

    Update pdfToSpeecg.py

--- pdfToSpeecg.py
+++ pdfToSpeecg.py
@@ -5,7 +5,7 @@ readfunc = pyttsx3.init()
 enterloc="enter the location of the pdf file you want me to read"
 readfunc.say(enterloc)
 readfunc.runAndWait()
-location=input(r'')
+location=input("enter:"r'')
 book = open(location,'rb')
 
 pdfread = PyPDF2.PdfFileReader(book)
